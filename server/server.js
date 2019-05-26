b0608e225c0c91f3dfef8d4a522d6e8f34780d4c
Anastasia Egorova <anastasia.eg2302@gmail.com>
2019-05-26 12:38:58 -0400

    Connect to firebase

--- server/server.js
+++ server/server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
 const app = express();
 const port = process.env.PORT || 5000;
+var serviceAccount = require('./secrets/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+var db = admin.firestore();
+db.collection('users').get()
+  .then((snapshot) => {
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
